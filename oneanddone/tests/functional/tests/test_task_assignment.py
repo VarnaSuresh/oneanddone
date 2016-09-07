93055304234216577c28170e48781d39ad06ee54
VarnaSuresh <varna96@gmail.com>
2016-09-07 23:29:54 +0530

    Fixed flake8 issues

--- oneanddone/tests/functional/tests/test_task_assignment.py
+++ oneanddone/tests/functional/tests/test_task_assignment.py
@@ -11,14 +11,14 @@ class TestAvailableTasks():
         home_page = HomePage(selenium, base_url).open()
         home_page.login(new_user)
         available_tasks_page = home_page.click_available_tasks()
-        home_page.search_for_task('\'' + assigned_task.name + '\'')
+        home_page.search_for_task(assigned_task.name)
         assert len(available_tasks_page.available_tasks) == 0
 
     def test_one_time_task_is_available(self, base_url, selenium, nonrepeatable_task, new_user):
         home_page = HomePage(selenium, base_url).open()
         home_page.login(new_user)
         available_tasks_page = home_page.click_available_tasks()
-        home_page.search_for_task('\'' + nonrepeatable_task.name + '\'')
+        home_page.search_for_task(nonrepeatable_task.name)
         assert len(available_tasks_page.available_tasks) == 1
 
         task = available_tasks_page.available_tasks[0]
