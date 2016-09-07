e45c78c40b01a048c192b8e3c614cd3a55577b0f
VarnaSuresh <varna96@gmail.com>
2016-09-07 20:35:26 +0530

    Added new test case

--- oneanddone/tests/functional/tests/test_task_assignment.py
+++ oneanddone/tests/functional/tests/test_task_assignment.py
@@ -13,14 +13,14 @@ class TestAvailableTasks():
         available_tasks_page = home_page.click_available_tasks()
         home_page.search_for_task(assigned_task['task'].name)
         available_tasks = [x.name for x in available_tasks_page.available_tasks]
-        assert available_tasks.count(assigned_task.name) == 0
+        assert available_tasks.count(assigned_task['task'].name) == 0
 
     def test_assigned_task_is_available_to_assignee(self, base_url, selenium, assigned_task):
         home_page = HomePage(selenium, base_url).open()
         new_user = {
             'email': assigned_task['user'].email,
             'password': assigned_task['user'].password,
-            'name': assigned_task['user'].name,
+            'name': assigned_task['user'].display_name,
             'username': assigned_task['user'].username,
             'url': 'http://www.mozilla.org/'
         }
