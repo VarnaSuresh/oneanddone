eb3cc813daaf6decd6c95edfa5d33e5db0751737
VarnaSuresh <varna96@gmail.com>
2016-09-07 23:27:29 +0530

    Fixed flake8 issues

--- oneanddone/tests/functional/tests/test_task_assignment.py
+++ oneanddone/tests/functional/tests/test_task_assignment.py
@@ -11,17 +11,17 @@ class TestAvailableTasks():
         home_page = HomePage(selenium, base_url).open()
         home_page.login(new_user)
         available_tasks_page = home_page.click_available_tasks()
-        home_page.search_for_task(assigned_task.name)
+        home_page.search_for_task('\'' + assigned_task.name + '\'')
         assert len(available_tasks_page.available_tasks) == 0
 
-    def test_assigned_task_is_available_to_assignee(self, base_url, selenium, nonrepeatable_task, new_user):
+    def test_one_time_task_is_available(self, base_url, selenium, nonrepeatable_task, new_user):
         home_page = HomePage(selenium, base_url).open()
         home_page.login(new_user)
         available_tasks_page = home_page.click_available_tasks()
-        home_page.search_for_task(nonrepeatable_task.name)
+        home_page.search_for_task('\'' + nonrepeatable_task.name + '\'')
+        assert len(available_tasks_page.available_tasks) == 1
 
         task = available_tasks_page.available_tasks[0]
-        task_name = task.name
         task_details = task.click()
         assert task_details.is_get_started_button_visible
         assert not task_details.is_save_for_later_button_visible
